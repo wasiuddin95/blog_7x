0034f610d7e69be5099f3401e9c0e26c9c971d44
wasiuddin95 <wuddin73@gmail.com>
2021-01-31 01:14:18 +0600

    Post info showing on post page

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -9,10 +9,18 @@ class FrontEndController extends Controller
 {
     public function home()
     {
-        $posts = Post::orderBy('created_at', 'DESC')->take(5)->get();
-        $recentPosts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
+        $posts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->take(5)->get();
+        $firstposts2 = $posts->splice(0,2);
+        $middleposts = $posts->splice(0,1);
+        $lastposts = $posts->splice(0);
 
-        return view('website.home', compact(['posts', 'recentPosts']));
+        $footerPosts = Post::with('category', 'user')->inRandomOrder()->limit(4)->get();
+        $firstFooterPosts = $footerPosts->splice(0,1);
+        $firstFooterPosts2 = $footerPosts->splice(0,2);
+        $lastFooterPost = $footerPosts->splice(0,1);
+
+        $recentPosts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
+        return view('website.home', compact(['posts', 'recentPosts', 'firstposts2', 'middleposts', 'lastposts', 'footerPosts', 'firstFooterPosts', 'firstFooterPosts2', 'lastFooterPost'])); 
     }
     
     public function about()
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -6,54 +6,44 @@
     <div class="container">
         <div class="row align-items-stretch retro-layout-2">
             <div class="col-md-4">
-                <a href="single.html" class="h-entry mb-30 v-height gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_1.jpg');">
+                @foreach ($firstposts2 as $post)
+                <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="h-entry mb-30 v-height gradient"
+                    style="background-image: url('{{ $post->image }}');">
 
                     <div class="text">
-                        <h2>The AI magically removes moving objects from videos.</h2>
-                        <span class="date">July 19, 2019</span>
+                        <h2>{{ $post->title }}</h2>
+                        <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                     </div>
                 </a>
-                <a href="single.html" class="h-entry v-height gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_2.jpg');">
-
-                    <div class="text">
-                        <h2>The AI magically removes moving objects from videos.</h2>
-                        <span class="date">July 19, 2019</span>
-                    </div>
-                </a>
+                @endforeach
             </div>
             <div class="col-md-4">
-                <a href="single.html" class="h-entry img-5 h-100 gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_v_1.jpg');">
+                @foreach ($middleposts as $post)
+                <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="h-entry img-5 h-100 gradient"
+                    style="background-image: url('{{ $post->image }}');">
 
                     <div class="text">
                         <div class="post-categories mb-3">
-                            <span class="post-category bg-danger">Travel</span>
-                            <span class="post-category bg-primary">Food</span>
+                            <span class="post-category bg-danger">{{ $post->category->name }}</span>
+                            {{-- <span class="post-category bg-primary">Food</span> --}}
                         </div>
-                        <h2>The AI magically removes moving objects from videos.</h2>
-                        <span class="date">July 19, 2019</span>
+                        <h2>{{ $post->title }}</h2>
+                        <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                     </div>
                 </a>
+                @endforeach
             </div>
             <div class="col-md-4">
-                <a href="single.html" class="h-entry mb-30 v-height gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_3.jpg');">
+                @foreach ($lastposts as $post)
+                <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="h-entry mb-30 v-height gradient"
+                    style="background-image: url('{{ $post->image }}');">
 
                     <div class="text">
-                        <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                        <span class="date">July 19, 2019</span>
+                        <h2>{{ $post->title }}</h2>
+                        <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                     </div>
                 </a>
-                <a href="single.html" class="h-entry v-height gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
-
-                    <div class="text">
-                        <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                        <span class="date">July 19, 2019</span>
-                    </div>
-                </a>
+                @endforeach
             </div>
         </div>
     </div>
@@ -66,7 +56,7 @@
                 <h2>Recent Posts</h2>
             </div>
         </div>
-        <div class="row">
+        <div class="row justify-content-between">
             @foreach ($recentPosts as $post)
             <div class="col-lg-4 mb-4">
                 <div class="entry2">
@@ -83,7 +73,8 @@
                             <span>&nbsp;-&nbsp; {{ $post->created_at->format('M d, Y') }}</span>
                         </div>
 
-                        <p> {{ Str::limit($post->description, 100) }} </p>
+                        {{-- <p> {{ Str::limit($post->description, 100) }} </p> --}}
+                        <p> {!! Str::limit($post->description, 100) !!} </p>
                         <p><a href="{{ route('website.post', ['slug' => $post->slug]) }}">Read More</a></p>
                     </div>
                 </div>
@@ -112,44 +103,40 @@
         <div class="row align-items-stretch retro-layout">
 
             <div class="col-md-5 order-md-2">
-                <a href="single.html" class="hentry img-1 h-100 gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
-                    <span class="post-category text-white bg-danger">Travel</span>
+                @foreach ($firstFooterPosts as $post)
+                <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="hentry img-1 h-100 gradient"
+                    style="background-image: url('{{ $post->image }}');">
+                    <span class="post-category text-white bg-danger">{{ $post->category->name }}</span>
                     <div class="text">
-                        <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                        <span>February 12, 2019</span>
+                        <h2>{{ $post->title }}</h2>
+                        <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                     </div>
                 </a>
+                @endforeach
             </div>
 
             <div class="col-md-7">
 
-                <a href="single.html" class="hentry img-2 v-height mb30 gradient"
-                    style="background-image: url('{{ asset('website') }}/images/img_1.jpg');">
-                    <span class="post-category text-white bg-success">Nature</span>
+                @foreach ($lastFooterPost as $post)
+                <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="hentry img-2 v-height mb30 gradient" style="background-image: url('{{ $post->image }}');">
+                    <span class="post-category text-white bg-success">{{ $post->category->name }}</span>
                     <div class="text text-sm">
-                        <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                        <span>February 12, 2019</span>
+                        <h2>{{ $post->title }}</h2>
+                        <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                     </div>
-                </a>
+                </a>    
+                @endforeach
 
-                <div class="two-col d-block d-md-flex">
-                    <a href="single.html" class="hentry v-height img-2 gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_2.jpg');">
-                        <span class="post-category text-white bg-primary">Sports</span>
+                <div class="two-col d-block d-md-flex justify-content-between">
+                    @foreach ($firstFooterPosts2 as $post)
+                    <a href="{{ route('website.post', ['slug' => $post->slug]) }}" class="hentry v-height img-2 gradient" style="background-image: url('{{ $post->image }}');">
+                        <span class="post-category text-white bg-primary">{{ $post->category->name }}</span>
                         <div class="text text-sm">
-                            <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                            <span>February 12, 2019</span>
+                            <h2>{{ $post->title }}</h2>
+                            <span class="date">{{ $post->created_at->format('M d, Y') }}</span>
                         </div>
-                    </a>
-                    <a href="single.html" class="hentry v-height img-2 ml-auto gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_3.jpg');">
-                        <span class="post-category text-white bg-warning">Lifestyle</span>
-                        <div class="text text-sm">
-                            <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                            <span>February 12, 2019</span>
-                        </div>
-                    </a>
+                    </a>    
+                    @endforeach
                 </div>
 
             </div>
