700b151a1d581563c13bb52679a86510253bd147
wasiuddin95 <wuddin73@gmail.com>
2021-02-03 14:26:18 +0600

    categories data shared with appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $categories = Category::take(5)->get();
+
+        View::share('categories', $categories);
     }
 }
